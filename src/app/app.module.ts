84acf8b4464ff58f1f091ed8cc73b9af0e59204c
V d M <vhmoura@gmail.com>
2017-09-26 17:16:56 -0300

    added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SucessAlertComponent } from './sucess-alert/sucess-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SucessAlertComponent
   ],
   imports: [
     BrowserModule
